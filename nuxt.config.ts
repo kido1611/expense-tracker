15c48fc9f874303e4b2bd1bec8f3612b53a9baec
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-09-29 07:47:57 +0700

    feat: add and enable nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxthub/core', '@nuxt/eslint'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui'],
   future: {
     compatibilityVersion: 4,
   },
