964a57c630e79319b12d977653ee4476cae83481
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-06 11:58:17 +0700

    deps: add nuxt-auth-utils dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui'],
+  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui", "nuxt-auth-utils"],
   future: {
     compatibilityVersion: 4,
   },
   hub: {
     blob: true,
     database: true,
-  }
-})
+  },
+  runtimeConfig: {
+    session: {
+      maxAge: 60 * 60 * 24, // 2 Hours
+    },
+  },
+});
