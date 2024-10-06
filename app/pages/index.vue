f46b02d6283c7a2ba03916755529220f6aa45279
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-06 19:41:01 +0700

    chore: update index page to accessible for logged in user only

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-const route = useRoute()
+definePageMeta({
+  middleware: "auth",
+});
+
+const { user, loggedIn, clear } = useUserSession();
+const route = useRoute();
+
+async function logout() {
+  await clear();
+
+  navigateTo("/login");
+}
 </script>
 
 <template>
   <div>
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+      >Learn more about Nuxt Routing</a
+    >
+
+    <p>
+      {{ loggedIn }}
+      {{ user }}
+    </p>
+
+    <UButton type="button" @click="logout">Logout</UButton>
   </div>
 </template>
