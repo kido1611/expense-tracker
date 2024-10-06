a412eac968e4afcca3a22dec2403229354bfb0c2
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-06 11:55:57 +0700

    feat: add database schema and migration files

---
+++ server/database/migrations/0000_unusual_magus.sql
@@ -0,0 +1,47 @@
+CREATE TABLE `categories` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`is_expense` integer DEFAULT false,
+	`created_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `transactions` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`wallet_id` integer NOT NULL,
+	`category_id` integer NOT NULL,
+	`nanoid` text NOT NULL,
+	`amount` integer DEFAULT 0 NOT NULL,
+	`note` text,
+	`is_visible_in_report` integer DEFAULT true,
+	`spend_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`created_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	FOREIGN KEY (`wallet_id`) REFERENCES `wallets`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`password` text NOT NULL,
+	`created_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `wallets` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`nanoid` text NOT NULL,
+	`name` text NOT NULL,
+	`balance` integer DEFAULT 0 NOT NULL,
+	`created_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `transactions_nanoid_unique` ON `transactions` (`nanoid`);--> statement-breakpoint
+CREATE INDEX `transactions_nanoid_idx` ON `transactions` (`nanoid`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE UNIQUE INDEX `wallets_nanoid_unique` ON `wallets` (`nanoid`);--> statement-breakpoint
+CREATE INDEX `wallets_nanoid_idx` ON `wallets` (`nanoid`);--> statement-breakpoint
+CREATE INDEX `wallets_name_idx` ON `wallets` (`name`);
---
+++ server/database/schema.ts
@@ -0,0 +1,106 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, integer, index } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const wallets = sqliteTable(
+  "wallets",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    nanoid: text("nanoid").notNull().unique(),
+    name: text("name").notNull(),
+    balance: integer("balance").notNull().default(0),
+    createdAt: integer("created_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`(CURRENT_TIMESTAMP)`),
+    updatedAt: integer("updated_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`(CURRENT_TIMESTAMP)`),
+  },
+  (table) => {
+    return {
+      nanoidIdx: index("wallets_nanoid_idx").on(table.nanoid),
+      nameIdx: index("wallets_name_idx").on(table.name),
+    };
+  },
+);
+
+export const categories = sqliteTable("categories", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  isExpense: integer("is_expense", { mode: "boolean" }).default(false),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const transactions = sqliteTable(
+  "transactions",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    walletId: integer("wallet_id")
+      .notNull()
+      .references(() => wallets.id),
+    categoryId: integer("category_id")
+      .notNull()
+      .references(() => categories.id),
+
+    nanoid: text("nanoid").notNull().unique(),
+    amount: integer("amount").notNull().default(0),
+    note: text("note"),
+    isVisibleInReport: integer("is_visible_in_report", {
+      mode: "boolean",
+    }).default(true),
+
+    spendAt: integer("spend_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`(CURRENT_TIMESTAMP)`),
+
+    createdAt: integer("created_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`(CURRENT_TIMESTAMP)`),
+    updatedAt: integer("updated_at", { mode: "timestamp" })
+      .notNull()
+      .default(sql`(CURRENT_TIMESTAMP)`),
+  },
+  (table) => {
+    return {
+      nanoidIdx: index("transactions_nanoid_idx").on(table.nanoid),
+    };
+  },
+);
+
+export const walletTransfers = sqliteTable("wallet_transfers", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  sourceWalletId: integer("source_wallet_id")
+    .notNull()
+    .references(() => wallets.id),
+  sourceTransactionId: integer("source_transaction_id")
+    .notNull()
+    .references(() => transactions.id),
+  targetWalletId: integer("target_wallet_id")
+    .notNull()
+    .references(() => wallets.id),
+  targetTransactionId: integer("target_transaction_id")
+    .notNull()
+    .references(() => transactions.id),
+  feeTransactionId: integer("fee_transaction_id").references(
+    () => transactions.id,
+  ),
+});
+
+// TODO: table ledger
