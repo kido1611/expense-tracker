8626a06a13b12854743e5e1a0e256bb16193cab2
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-20 07:01:37 +0700

    feat: add icon in wallet form

--- app/components/WalletForm.vue
+++ app/components/WalletForm.vue
@@ -8,7 +8,71 @@ type Schema = z.output<typeof walletSchema>;
 const state = reactive({
   name: "",
   balance: 0,
+  icon: "i-tabler-wallet",
 });
+
+const selectedIcon = computed(() => {
+  if (!state.icon) {
+    return null;
+  }
+
+  return icons.filter((data) => data.icon === state.icon)[0];
+});
+
+const icons = [
+  {
+    label: "Wallet",
+    icon: "i-tabler-wallet",
+  },
+  {
+    label: "Cash",
+    icon: "i-tabler-cash",
+  },
+  {
+    label: "Cash Banknote",
+    icon: "i-tabler-cash-banknote",
+  },
+  {
+    label: "Bank Building",
+    icon: "i-tabler-building-bank",
+  },
+  {
+    label: "Pig",
+    icon: "i-tabler-pig-money",
+  },
+  {
+    label: "Moneybag",
+    icon: "i-tabler-moneybag",
+  },
+  {
+    label: "Credit Card",
+    icon: "i-tabler-credit-card",
+  },
+  {
+    label: "Briefcase",
+    icon: "i-tabler-briefcase",
+  },
+  {
+    label: "Shopping Cart",
+    icon: "i-tabler-shopping-cart",
+  },
+  {
+    label: "Lock",
+    icon: "i-tabler-lock",
+  },
+  {
+    label: "Heart",
+    icon: "i-tabler-heart",
+  },
+  {
+    label: "Brand: MasterCard",
+    icon: "i-tabler-brand-mastercard",
+  },
+  {
+    label: "Brand: Visa",
+    icon: "i-tabler-brand-visa",
+  },
+];
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   isLoading.value = true;
@@ -20,10 +84,14 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
     .then(() => {
       state.name = "";
       state.balance = 0;
+      state.icon = "i-tabler-wallet";
 
       refreshNuxtData("wallets");
+      refreshNuxtData("latest-transactions");
     })
-    .catch((_err) => {})
+    .catch((_err) => {
+      // TODO: error on backend validation
+    })
     .finally(() => {
       isLoading.value = false;
     });
@@ -40,7 +108,7 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
       class="flex flex-col space-y-5"
       @submit="onSubmit"
     >
-      <UFormGroup label="Name" name="name">
+      <UFormGroup label="Name" name="name" required>
         <UInput
           v-model="state.name"
           type="text"
@@ -48,7 +116,7 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
           :disabled="isLoading"
         />
       </UFormGroup>
-      <UFormGroup label="Balance" name="balance">
+      <UFormGroup label="Balance" name="balance" required>
         <UInput
           v-model="state.balance"
           type="number"
@@ -56,6 +124,27 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
           min="0"
           :disabled="isLoading"
         />
+      </UFormGroup>
+      <UFormGroup label="Icon" name="icon" required>
+        <USelectMenu
+          v-model="state.icon"
+          :options="icons"
+          option-attribute="label"
+          value-attribute="icon"
+          searchable
+          searchable-placeholder="Find icon..."
+          clear-search-on-close
+          placeholder="Find icon"
+          required
+          :disabled="isLoading"
+        >
+          <template #label>
+            <template v-if="selectedIcon">
+              <UIcon :name="selectedIcon.icon" class="flex-none size-5" />
+              <p class="">{{ selectedIcon.label }}</p>
+            </template>
+          </template>
+        </USelectMenu>
       </UFormGroup>
       <UButton
         type="submit"
