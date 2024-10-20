a6398c96fdadb260aadf041455e8e880bddea06e
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-20 07:02:46 +0700

    chore: update index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import type { Wallet, Transaction } from "~/types";
+
 definePageMeta({
   middleware: "auth",
 });
+
+const { data: walletsData } = await useFetch<Wallet[]>("/api/wallets", {
+  key: "wallets",
+});
+
+const { data: transactionsData } = await useFetch<Transaction[]>(
+  "/api/transactions",
+  {
+    key: "latest-transactions",
+  },
+);
 
 const { user, loggedIn, clear } = useUserSession();
 const route = useRoute();
@@ -30,6 +43,33 @@ async function logout() {
       >Logout</UButton
     >
 
-    <WalletForm />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8 py-8">
+      <div>
+        <WalletForm />
+        <div
+          v-if="walletsData ? walletsData.length > 0 : false"
+          class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 mt-8 rounded-lg overflow-hidden bg-neutral-900"
+        >
+          <WalletItem
+            v-for="wallet in walletsData"
+            :key="wallet.nanoid"
+            :wallet
+          />
+        </div>
+      </div>
+      <div>
+        <TransactionForm />
+        <div
+          v-if="transactionsData ? transactionsData.length > 0 : false"
+          class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 mt-8 rounded-lg overflow-hidden bg-neutral-900"
+        >
+          <TransactionItem
+            v-for="transaction in transactionsData"
+            :key="transaction.nanoid"
+            :transaction
+          />
+        </div>
+      </div>
+    </div>
   </UContainer>
 </template>
