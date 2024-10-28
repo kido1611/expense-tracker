3715ea1b91912116782f25afc62d383de25b5cbe
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-10-28 07:10:41 +0700

    fix: delete related transactions when have wallet-transfer

--- server/api/transactions/[nanoid]/index.delete.ts
+++ server/api/transactions/[nanoid]/index.delete.ts
@@ -1,10 +1,11 @@
 import type { H3Event } from "h3";
-import { eq, and } from "drizzle-orm";
+import { eq, and, inArray } from "drizzle-orm";
 import { transactionRouteParamSchema } from "~/utils/zodSchema";
 import {
   deleteTransactionImage,
   getUserTransactionByNanoid,
 } from "~~/server/utils/transaction";
+import { getWalletTransferByTransactionId } from "~~/server/utils/walletTransfer";
 
 export default defineEventHandler(async (event: H3Event) => {
   const validatedParams = await getValidatedRouterParams(
@@ -20,49 +21,65 @@ export default defineEventHandler(async (event: H3Event) => {
     validatedParams.nanoid,
   );
 
-  const wallet = await useDrizzle().query.wallets.findFirst({
-    where: and(
-      eq(tables.wallets.userId, user.id),
-      eq(tables.wallets.id, transaction.walletId),
-    ),
-  });
+  const walletTransfer = await getWalletTransferByTransactionId(transaction.id)
 
-  if (!wallet) {
-    throw createError({
-      statusCode: 404,
-      message: "Wallet not found",
+  const deleteTransactions = [];
+  const updateWallets = new Map<number, number>();
+
+  if (!walletTransfer) {
+    deleteTransactions.push(transaction.id)
+
+    updateWallets.set(transaction.walletId, transaction.realAmount * -1)
+
+    await deleteTransactionImage(transaction)
+  } else {
+    const transactionIds = [
+      walletTransfer.sourceTransactionId, walletTransfer.targetTransactionId,
+      ...(walletTransfer.feeTransactionId ? [walletTransfer.feeTransactionId] : [])
+    ];
+
+    const transactions = await useDrizzle().query.transactions.findMany({
+      where: and(
+        eq(tables.transactions.userId, user.id),
+        inArray(tables.transactions.id, transactionIds)
+      )
+    })
+
+    transactions.forEach(item => {
+      deleteTransactions.push(item.id)
+      updateWallets.set(item.walletId, (updateWallets.get(item.walletId) ?? 0) - item.realAmount)
     });
+
+    await useDrizzle().delete(tables.walletTransfers)
+      .where(eq(tables.walletTransfers.id, walletTransfer.id))
+
+    // TODO: Delete image transaction if exist
   }
 
-  const result = await useDrizzle()
-    .delete(tables.transactions)
-    .where(
-      and(
-        eq(tables.transactions.userId, user.id),
-        eq(tables.transactions.nanoid, validatedParams.nanoid),
-      ),
-    )
-    .returning({
-      nanoid: tables.transactions.nanoid,
-      amount: tables.transactions.amount,
-      note: tables.transactions.note,
-      image_path: tables.transactions.imagePath,
-      spend_at: tables.transactions.spendAt,
-      is_visible_in_report: tables.transactions.isVisibleInReport,
-      created_at: tables.transactions.createdAt,
-    });
+  await useDrizzle().delete(tables.transactions)
+    .where(and(
+      inArray(tables.transactions.id, deleteTransactions),
+      eq(tables.transactions.userId, user.id)
+    ))
 
-  await deleteTransactionImage(transaction);
+  updateWallets.forEach(async (value, key) => {
+    await useDrizzle().update(tables.wallets)
+      .set({
+        balance: sql`${tables.wallets.balance} + ${value}`
+      })
+      .where(and(
+        eq(tables.wallets.userId, user.id),
+        eq(tables.wallets.id, key)
+      ))
+  })
 
-  await useDrizzle()
-    .update(tables.wallets)
-    .set({
-      balance: wallet.balance - transaction.realAmount,
-    })
-    .where(eq(tables.wallets.id, wallet.id));
-
-  // TODO: get wallet transfers
-  // TODO: fix jika transaki ada wallet transfer
-
-  return result;
+  return {
+    nanoid: transaction.nanoid,
+    amount: transaction.amount,
+    note: transaction.note,
+    image_path: transaction.imagePath,
+    spend_at: transaction.spendAt,
+    is_visible_in_report: transaction.isVisibleInReport,
+    created_at: transaction.createdAt,
+  }
 });
