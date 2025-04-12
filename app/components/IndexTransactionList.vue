853b1b3a889ebfe878bfa65e2fc55c2a883b3817
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-04-13 06:07:15 +0700

    fix: skeleton does not show when data is empty

--- app/components/IndexTransactionList.vue
+++ app/components/IndexTransactionList.vue
@@ -7,30 +7,41 @@ const { data, status } = useFetch<Transaction[]>("/api/transactions", {
     limit: 5,
   },
 });
+
+const isSkeletonVisible = computed(() => {
+  return (
+    status.value === "pending" &&
+    (!data.value || (data.value && data.value.length === 0))
+  );
+});
 </script>
 
 <template>
   <div class="relative">
     <h2 class="text-lg font-medium mb-4">Transactions</h2>
 
-    <div
-      v-if="status == 'pending' && data && data.length === 0"
-      class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
-    >
-      <LazyTransactionItemSkeleton v-for="i in 5" :key="i" />
-    </div>
-    <div
-      v-if="data ? data.length > 0 : false"
-      class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
-    >
-      <TransactionItem
-        v-for="transaction in data"
-        :key="transaction.nanoid"
-        :transaction
-      />
-    </div>
-    <div v-else class="text-neutral-400 text-center py-16">
-      Transaction is empty
-    </div>
+    <template v-if="isSkeletonVisible">
+      <div
+        class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
+      >
+        <LazyTransactionItemSkeleton v-for="i in 5" :key="i" />
+      </div>
+    </template>
+
+    <template v-else>
+      <div
+        v-if="data ? data.length > 0 : false"
+        class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
+      >
+        <TransactionItem
+          v-for="transaction in data"
+          :key="transaction.nanoid"
+          :transaction
+        />
+      </div>
+      <div v-else class="text-neutral-400 text-center py-16">
+        Transaction is empty
+      </div>
+    </template>
   </div>
 </template>
--- app/components/IndexWalletList.vue
+++ app/components/IndexWalletList.vue
@@ -7,6 +7,13 @@ const { data, status } = useFetch("/api/wallets", {
   key: INDEX_WALLETS_CACHE_KEY_NAME,
 });
 
+const isSkeletonVisible = computed(() => {
+  return (
+    status.value === "pending" &&
+    (!data.value || (data.value && data.value.length === 0))
+  );
+});
+
 const openWalletTransfer = (walletNanoid: string) => {
   emit("transfer", walletNanoid);
 };
@@ -22,24 +29,30 @@ const openWalletTransfer = (walletNanoid: string) => {
     <!-- /> -->
     <h2 class="text-lg font-medium mb-4">Wallets</h2>
 
-    <div
-      v-if="status == 'pending' && data && data.length === 0"
-      class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
-    >
-      <LazyWalletItemSkeleton v-for="i in 5" :key="i" />
-    </div>
-    <div
-      v-if="data ? data.length > 0 : false"
-      class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
-    >
-      <WalletItem
-        v-for="wallet in data"
-        :key="wallet.nanoid"
-        :wallet="wallet"
-        @transfer="openWalletTransfer"
-      />
-    </div>
+    <template v-if="isSkeletonVisible">
+      <div
+        class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
+      >
+        <LazyWalletItemSkeleton v-for="i in 5" :key="i" />
+      </div>
+    </template>
 
-    <div v-else class="text-center py-16 text-neutral-400">Wallet is empty</div>
+    <template v-else>
+      <div
+        v-if="data ? data.length > 0 : false"
+        class="grid grid-cols-1 border border-neutral-700 divide-y divide-neutral-700 rounded-lg overflow-hidden bg-neutral-900"
+      >
+        <WalletItem
+          v-for="wallet in data"
+          :key="wallet.nanoid"
+          :wallet="wallet"
+          @transfer="openWalletTransfer"
+        />
+      </div>
+
+      <div v-else class="text-center py-16 text-neutral-400">
+        Wallet is empty
+      </div>
+    </template>
   </div>
 </template>
