7e5e8a74ed0b3ee7cff85cb39183c7292dbfa520
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-04-24 15:25:30 +0700

    chore: add NuxtHub (d1) database type

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/d1";
-// import type {DrizzleD1Database} from "drizzle-orm/d1";
+import type { DrizzleD1Database } from "drizzle-orm/d1";
 import * as schema from "../database/schema";
 
 export type { DrizzleD1Database } from "drizzle-orm/d1";
@@ -17,6 +17,7 @@ export type Category = typeof schema.categories.$inferSelect;
 export type Transaction = typeof schema.transactions.$inferSelect;
 export type WalletTransfer = typeof schema.walletTransfers.$inferSelect;
 
+export type DrizzleDatabase = DrizzleD1Database<typeof schema>;
 // export type DBTransaction = Parameters<
 //   Parameters<DrizzleD1Database<typeof schema>["transaction"]>[0]
 // >[0];
