4abbdb026b5e6a2381cfc1534020b2c34e415627
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-04-26 23:33:17 +0700

    chore: add dashboard tile and other layout in dashboard index page

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -2,18 +2,36 @@
 definePageMeta({
   middleware: "auth",
   layout: "dashboard",
+  title: "Home",
 });
 </script>
 
 <template>
-  <UContainer>
-    <div class="mt-8 flex flex-row flex-wrap gap-4">
+  <UContainer class="py-8">
+    <div class="mb-6 flex flex-row flex-wrap gap-4">
       <WalletSlideover />
       <TransactionSlideover />
       <WalletTransferSlideover />
     </div>
 
-    <div class="grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-6 py-8">
+    <div class="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6">
+      <LazyDashboardTotalBalanceTile hydrate-on-idle />
+    </div>
+    <div class="mt-6 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6">
+      <LazyDashboardTotalIncomeTile hydrate-on-idle />
+      <LazyDashboardTotalExpenseTile hydrate-on-idle />
+      <LazyDashboardNetworthTile hydrate-on-idle />
+    </div>
+
+    <div
+      class="mt-6 grid grid-cols-1 gap-6 *:h-[400px] *:rounded-md *:border *:border-red-400 *:p-4 xl:grid-cols-[1fr_350px]"
+    >
+      <div>Chart in latest year</div>
+      <div>
+        Expense per Cateogory in this month, show message when no expense
+      </div>
+    </div>
+    <div class="mt-6 grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
       <IndexWalletList />
       <IndexTransactionList />
     </div>
