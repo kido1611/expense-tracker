f73d86881dbc3b87103e32b199e1d62d2948887a
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-06-06 20:25:25 +0700

    chore: remove nuxt/eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "@nuxthub/core",
-    "@nuxt/eslint",
     "@nuxt/ui",
     "nuxt-auth-utils",
     "@nuxt/test-utils/module",
