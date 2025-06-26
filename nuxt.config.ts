ef0ae3e1d6cd36a1fbcb979aae0623ec2ee95c25
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-06-26 22:50:25 +0700

    chore: redirect to dashboard when accessing home

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,9 @@ export default defineNuxtConfig({
     preference: "dark",
   },
   routeRules: {
+    "/": {
+      redirect: "/dashboard",
+    },
     "/dashboard/**": {
       ssr: false,
     },
